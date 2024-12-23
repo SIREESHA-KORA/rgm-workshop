277d91647400369ddb3edde2a480dd6dc01b3a0d
Kora Sireesha <korasireesha1@gmail.com>
2024-12-23 14:19:30 +0530

    web page

---
+++ javascript.js
@@ -0,0 +1,4 @@
+// JavaScript function to change the text when the button is clicked
+document.getElementById("changeTextButton").addEventListener("click", function() {
+    document.getElementById("textToChange").innerHTML = "The text has been changed! You clicked the button.";
+});
