67392c617fd5b6c061f4197f5233b2984d3a5d53
Kora Sireesha <korasireesha1@gmail.com>
2025-01-03 22:12:36 +0530

    average

---
+++ average.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int s1=67,s2=87,s3=59,s4=99,s5=90,s6=100;
+    float avg;
+    avg=(s1+s2+s3+s4+s5+s6)/6.0;
+    printf("\n average =%f",avg);
+    return 0;
+}
