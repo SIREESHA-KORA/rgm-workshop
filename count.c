e927ecb34f5ea327ec79a171e009112bb6dcedc8
Kora Sireesha <korasireesha1@gmail.com>
2025-01-05 21:58:12 +0530

    count

---
+++ count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    long int number;
+    scanf("%ld",&number);
+    int count=0;
+    while(number){
+        count++;
+        number=numbrer/10;
+ }
+printf("%d",count);
+return 0;
+
+}
